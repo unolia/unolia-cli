e7c156784530b0fb736564825fe8e4ca5904cce8
Eser DENIZ <srwiez@gmail.com>
2024-05-08 11:05:38 +0200

    New API URL

--- config/settings.php
+++ config/settings.php
@@ -24,6 +24,6 @@ return [
         | The base URL for the Unolia API.
         |
         */
-        'url' => env('UNOLIA_API_URL', 'https://api.unolia.com/v1/'),
+        'url' => env('UNOLIA_API_URL', 'https://app.unolia.com/api/v1/'),
     ],
 ];
